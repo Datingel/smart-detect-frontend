ab5d24089ccab23fa0fe81130830660cb1aa5cd3
Raphael Lautenbaer <raphael.lautenbacher@googlemail.com>
2019-08-03 16:16:33 +0200

    added build for deploying on netlify

---
+++ build/precache-manifest.a5e1bcfe92201627d938316d9d45330f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afac48c04efd90125ba350fcf99ce6e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9849b7f3305c8ce7ce94",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "edc03bbb9148ead48197",
+    "url": "/static/css/main.e48c2103.chunk.css"
+  },
+  {
+    "revision": "9849b7f3305c8ce7ce94",
+    "url": "/static/js/2.d3b94a2f.chunk.js"
+  },
+  {
+    "revision": "edc03bbb9148ead48197",
+    "url": "/static/js/main.bcb92918.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
